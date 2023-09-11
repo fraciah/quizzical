b84cb7184efa12f6538431cd7f4149b2e2dcba0b
fraciah <karfraciah@gmail.com>
2023-09-11 19:32:23 +0300

    Added isHeld property to answer

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 
 //decoding the HTML entities in the questions and answers, so that they are displayed correctly
@@ -15,56 +15,76 @@ function decodeHTMLEntities(text) {
     return text.replace(/&[^;]+;/g, match => entities[match] || match);
 }
 
-export default function Questions({quizFinished}){
+export default function Questions({ quizFinished }) {
     const [questions, setQuestions] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-        .then(response =>response.json())
-        .then(data => {
-            const {results} = data;
-            console.log(results);
-            setQuestions(results);
-        })
+            .then(response => response.json())
+            .then(data => {
+                const { results } = data;
+                console.log("Results:",results);
+                // adding an 'isHeld' property to each answer
+                const questionsWithAnswers = results.map(question => {
+                    const answers = [question.correct_answer, ...question.incorrect_answers].map(answer => ({
+                        answer: answer,
+                        isHeld: false
+                    }));
+                    return {
+                        // return a new object with all properties of the original question and an additional 'answers' property
+                        ...question,
+                        answers: answers
+                    };
+                });
+                setQuestions(questionsWithAnswers);
+            })
     }, []);
 
-    function handleClick(){
-        console.log("flipping");
+    function handleClick(questionIndex, answerIndex) {
+        setQuestions(prevQuestions => {
+            // console.log("questionIndex",questionIndex)
+            // console.log("answerIndex",answerIndex)
+            return prevQuestions.map((question, qIndex) => {
+                if (qIndex === questionIndex) {
+                    const newAnswers = question.answers.map((answer, aIndex) => {
+                        if (aIndex === answerIndex) {
+                            return { ...answer, isHeld: !answer.isHeld }; // Toggle isHeld for the clicked answer
+                        }
+                        return answer;
+                    });
+                    return { ...question, answers: newAnswers };
+                }
+                return question;
+            });
+        });
     }
 
-    return(
+    return (
         <div className="questions">
             <h1>Questions</h1>
             <ol>
-                {questions.map(question =>{
-                    //using Fisher-Yates algorithm to shuffle the answers
-                    //also adding a property isHeld to each answer
-                    const answers = [question.correct_answer, ...question.incorrect_answers].map(answer => ({answer: answer, 
-                                                                                                             isHeld: false}));
-                    for(let i=answers.length-1; i > 0; i--){
-                        const j = Math.floor(Math.random() * (i+1));
-                        [answers[i], answers[j]] = [answers[j], answers[i]];
-                    }
-                    return(
-                        //each question has a unique key 
-                        <li key={crypto.randomUUID()}>
-                            <h3>{decodeHTMLEntities(question.question)}</h3>
-                            <div className="answers">
-                                {answers.map(answer =>(
-                                    <div key={crypto.randomUUID()}
-                                         className={answer.isHeld? "answer-held" : "answer"}
-                                         onClick={handleClick}
-                                        >{decodeHTMLEntities(answer.answer)}{answer.isHeld.toString()}</div>
-                                ))}
-                            </div>
-                        </li>
-                    )
-                })}
+                {questions.map((question, questionIndex) => (
+                    <li key={questionIndex}>
+                        <h3>{decodeHTMLEntities(question.question)}</h3>
+                        <div className="answers">
+                            {question.answers.map((answer, answerIndex) => (
+                                <div
+                                    key={answerIndex}
+                                    className={answer.isHeld ? "answer-held" : "answer"}
+                                    onClick={() => handleClick(questionIndex, answerIndex)}
+                                >
+                                    {decodeHTMLEntities(answer.answer)} {answer.isHeld.toString()}
+                                </div>
+                            ))}
+                        </div>
+                    </li>
+                ))}
             </ol>
-            <button className={quizFinished? "won-button": "play-button"}>Check answers</button>
+            <button className={quizFinished ? "won-button" : "play-button"}>Check answers</button>
         </div>
     )
 }
+
 Questions.propTypes = {
     quizFinished: PropTypes.bool.isRequired,
     setQuizFinished: PropTypes.func.isRequired
