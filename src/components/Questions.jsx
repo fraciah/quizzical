dc0a20b1efc976028a19ff7f2567c6cf5a113b88
fraciah <karfraciah@gmail.com>
2023-10-04 19:42:28 +0300

    Adding my tag

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -144,6 +144,7 @@ export default function Questions({ quizFinished, setQuizFinished }) {
             >
                 {quizFinished && checkClicked ? "Play again" : "Check answers"}
             </button>
+            <a href="https://github.com/fraciah?tab=repositories" className="tag" target="blank">Crafted with 💖 by Fraciah</a>
         </div>
     )
 }
